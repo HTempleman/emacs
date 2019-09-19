f636f844d1fe68aa590ff745d4883c10d9dba84b
MTempleman <huntertempleman@gmail.com>
2019-09-18 18:31:20 -0600

    Finally Fixed Leader Key Issues
    The "SPC" leader key exists in a tenuous harmony with the
    correspondent  evil keybindings. I clearly need to give both
    the evil guide and the general.el documentation a more thorough
    read! I am still frustrated that I'm unable to map the Magit commit
    command, so C-c C-c will have to suffice for the time being.
    
    Got react mode tied in with typescript, company, js2, etc. It appears
    I have finally hit a point where I can stop tinkering.

--- init.el
+++ init.el
@@ -12,6 +12,10 @@
 ;; Enable auto-fill-mode
 (auto-fill-mode)
 
+;; Global Settings
+(show-paren-mode 1)
+(electric-pair-mode)
+
 ;; Bootstrap use-package:
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -41,25 +45,142 @@
 
 (use-package evil
 	     :ensure t
+	     :init
+	     (setq evil-want-C-u-scroll t)
 	     :config
-	     (evil-mode 1))
+	     (evil-mode 1)
+	     )
 
 (use-package evil-escape
 	     :ensure t
 	     :config
 	     (evil-escape-mode))
 
+(use-package js2-mode
+  :ensure t)
+
+(use-package typescript-mode
+  :ensure t)
+
 (use-package fzf
 	    :ensure t)
 
+
+(use-package async
+  :ensure t)
+
+(use-package helm
+  :ensure t
+  :init
+  :config
+  (helm-mode 1))
+
+(use-package helm-rg
+  :ensure t)
+
+(use-package projectile
+  :ensure t
+  :init
+  (setq projectile-require-project-root nil)
+  :config
+  (projectile-mode 1))
+
+(use-package helm-projectile
+  :ensure t
+  :init
+  (setq helm-projectile-fuzzy-match t)
+  :config
+  (helm-projectile-on))
+
+
 (use-package neotree
   :ensure t)
+
 
 (use-package magit
   :defer t)
 (use-package evil-magit
   :defer t)
-  
+
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'typescript-mode-hook 'flycheck-mode))
+
+;; Settings for React w/ Typescript
+;; Greatly helped along by a config from one daniel-vu.
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-h1-identifier-mode +1)
+  (company-mode +1))
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-show-numbers t)
+  (setq company-toolkit-align-annotations t)
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode))
+
+(use-package company-quickhelp
+  :ensure t
+  :init
+  (company-quickhelp-mode 1)
+  (use-package pos-tip
+    :ensure t))
+
+
+(use-package web-mode
+  :ensure t
+  :mode (("\\.html?\\'" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2
+	web-mode-css-indent-offset 2
+	web-mode-code-indent-offset 2
+	web-mode-block-padding 2
+	web-mode-comment-style 2
+
+	web-mode-enable-css-colorization t
+	web-mode-enable-auto-pairing t
+	web-mode-enable-comment-keywords t
+	web-mode-enable-current-element-highlight t
+	)
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
+
+
+(use-package rjsx-mode
+  :ensure t
+  :mode (("\\.tsx\\'" . rjsx-mode))
+  :config
+  (add-hook 'rjsx-mode-hook
+	    (lambda ()
+	      (when (string-equal "tsx" (file-name-extension buffer-file-name))
+		(setup-tide-mode))))
+  (flycheck-add-mode 'typescript-tslint 'rjsx-mode))
+
+(use-package typescript-mode
+  :ensure t
+  :config
+  (setq typescript-indent-level 2)
+  (add-hook 'typescript-mode #'subword-mode))
+
+
+(use-package tide
+  :init
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+	 (typescript-mode . tid-h1-identifier-mode)
+	 (before-save . tide-format-before-save)))
+		   
+
+;; Settings for which-key (Spacemacse emulation)
 
 (use-package which-key
   :ensure t
@@ -75,29 +196,31 @@
 (use-package general
   :ensure t
   :init
-  (setq general-override-states '(insert
-				  emacs
-				  hybrid
-				  normal
-				  visual
-				  motion
-				  operator
-				  replace))
   :config
   (general-define-key
-   :states '(normal visual insert emacs)
+   :keymaps '(emacs normal visual hybrid operator replace )
    :prefix "SPC"
-   :non-normal-prefix "M-SPC"
+   :non-normal-prefix " SPC"
    "TAB" '(switch-to-prev-buffer
 	   :which-key "previous-buffer")
 
+   "/" '(helm-projectile-rg
+	 :which-key "ripgrep")
+
+   ;; Bindings for projectile featurs.
+   "p" '("projectile")
+   "pf" '(helm-projectile-find-file
+	  :which-key "find files")
+   "pp" '(helm-projectile-switch-project
+	  :which-key "switch project")
 
    ;; Bindings for file search and management.
    "f" '("files")
-   "ff" '(fzf :which-key "fuzzy-file-search")
+   "ff" '(helm-find-files :which-key "helm-file-search")
    "ft" '(neotree-toggle :which-key "neotree-toggle")
    "fF" '(find-file :which-key "emacs-find-file")
    "fL" '(load-file :which-key "load-file")
+
 
    ;; Bindings for window positioning.
    "w" '("window")
@@ -115,6 +238,7 @@
    "gs" '(magit-status :which-key "status")
    "gi" '(magit-init :which-key "initialize-repo")
    "gc" '(magit-commit :which-key "commit")
+   "gC" '(magit-clone-regular :which-key "clone-regular")
 
    ;; Toggle keybindings
    "t" '("toggle")
@@ -133,6 +257,9 @@
     "R" 'neotree-change-root
     "h" 'neotree-collapse-all
     )
+
+  (general-def 'normal helm-map
+    "C-j" 'helm-next-line)
   )
 
 
@@ -143,7 +270,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-magit zenburn-theme which-key use-package restart-emacs neotree magit general fzf evil-escape evil auto-package-update))))
+    (helm rjsx-mode tide web-mode company-quickhelp company flycheck typescript-mode slime pdf-tools evil-magit zenburn-theme which-key use-package restart-emacs neotree magit general fzf evil-escape evil auto-package-update))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
