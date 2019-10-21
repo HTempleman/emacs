e7d54d0a8a5a89c25f5001d8f68bf13d33e36fb4
MTempleman <huntertempleman@gmail.com>
2019-10-20 19:14:06 -0600

    Added features.
    Now I can switch windows on the screen and open files based on point
    locations on paths.

--- init.el
+++ init.el
@@ -113,12 +113,90 @@
 (use-package all-the-icons
   :load-path "~/.emacs.d/git-packages/all-the-icons.el")
 
-(use-package neotree
-  :ensure t
-  :init
-  (setq neo-smart-open t)
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-follow-delay             0.2
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-desc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-width                         35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after treemacs evil
+  :ensure t)
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(use-package buffer-move
+  :ensure t
+  )
 
 (use-package magit
   :defer t)
@@ -223,7 +301,7 @@
   :config
   (general-override-mode)
   (general-define-key
-   :states 'normal 
+   :states '(normal treemacs)
    :keymaps 'override
    :prefix "SPC"
    :non-normal-prefix "SPC"
@@ -234,17 +312,20 @@
 
    ;; Bindings for projectile featurs.
    "p" '("projectile")
-   "pf" '(helm-projectile-find-file
-	  :which-key "find files")
+   "pf" '(helm-projectile-find-file-dwim-other-window
+	  :which-key "find-files")
    "pp" '(helm-projectile-switch-project
-	  :which-key "switch project")
+	  :which-key "switch-project")
 
    ;; Bindings for file search and management.
    "f" '("files")
    "ff" '(helm-find-files :which-key "helm-file-search")
-   "ft" '(neotree-toggle :which-key "neotree-toggle")
+   "ft" '(treemacs :which-key "treemacs")
    "fF" '(find-file :which-key "emacs-find-file")
    "fL" '(load-file :which-key "load-file")
+   "fa" '(treemacs-add-project-to-workspace :which-key "treemacs-add-project")
+   "fw" '(treemacs-switch-workspace :which-key "treemacs-switch-workspace")
+   "fC" '(treemacs-edit-workspaces :which-key "edit-workspace-config")
 
 
    ;; Bindings for window positioning.
@@ -257,6 +338,10 @@
    "wl" '(windmove-right :which-key "select-right-window")
    "wk" '(windmove-up :which-key "select-upper-window")
    "wj" '(windmove-down :which-key "select-lower-window")
+   "wH" '(buf-move-left :which-key "move-window-left")
+   "wL" '(buf-move-right :which-key "move-window-right")
+   "wK" '(buf-move-up :which-key "move-window-up")
+   "wJ" '(buf-move-down :which-key "move-window-down")
 
    ;; Bindings for Magit
    "g" '("magit")
